a7bc05304a12a1e03adac29b38a19d62d295fa23
Кирилл <kirill.r.2010.stud@gmail.com>
2017-11-16 00:03:11 +0300

    Сделал появление и скрытие модального окна на js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+function show_modal(event) {
+	modal_win.classList.add('open');
+	event.preventDefault();
+}
+
 function close_modal(modal_win) {
 	modal_win.classList.remove('open');
 }
@@ -5,4 +10,8 @@ function close_modal(modal_win) {
 var modal_win = document.getElementById('modal');
 var modal_btn = document.getElementById('modal_close');
 
-modal_btn.addEventListener("click", close_modal(modal_win));
+modal_btn.addEventListener('click', function() { close_modal(modal_win); } );
+
+
+var btn_write_us = document.getElementById('btn_write_us');
+btn_write_us.addEventListener('click', show_modal);
